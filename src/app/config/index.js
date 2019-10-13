60bcd04ae63447b15b5c471b3e0805157e103d27
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-10-13 13:42:23 +0530

    Server URL updated.

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  BACKEND_SERVER: 'http://localhost',
+  BACKEND_SERVER: window.location.hostname.indexOf('localhost') !== -1 ? 'http://localhost' : 'https://todo-blastoise.herokuapp.com',
   GOOGLE_CLIENT_ID: '595981444577-j9jv1jjbgn0iba1th87lcmmqnll83stc.apps.googleusercontent.com'
 }
 

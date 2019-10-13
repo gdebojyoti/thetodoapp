3814d885b88b7fb3a548fc1a773a5ebc0aa08dcf
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-10-13 12:07:52 +0530

    Data sync complete.

--- src/app/actions/todo.js
+++ src/app/actions/todo.js
@@ -66,6 +66,60 @@ export function syncTodos () {
     .then(response => response.json())
     .then(json => {
       console.log('todos', json)
+      // if successful, update local data
+      if (typeof json.todos === 'object') {
+        updateLocalTodos(json.todos)
+      }
     })
     .catch(err => console.warn('err', err))
+}
+
+function updateLocalTodos (sArr) {
+  if (!sArr.length) {
+    return
+  }
+
+  // fetch array of todos from local storage
+  const cArr = getValue('todos') || []
+
+  // compare cArr with sArr; generate new list of todos
+  cArr.forEach(todo => {
+    // check for invalid todo item
+    if (!todo || typeof todo !== 'object' || !todo.id) {
+      return
+    }
+
+    const index = sArr.findIndex(item => item.id === todo.id)
+    // if todo is new, push it to server todo array
+    if (index === -1) {
+      sArr.push({ ...todo })
+      return
+    }
+
+    let itemInServerData = sArr[index]
+
+    // ignore if item on server is already deleted
+    if (itemInServerData.isDeleted) {
+      return
+    }
+
+    // if todo has just been deleted, delete entire server item except id & isDeleted
+    if (todo.isDeleted) {
+      itemInServerData = {
+        id: todo.id,
+        isDeleted: true
+      }
+    }
+
+    // if client todo is more recent, replace server todo with it
+    if (!itemInServerData.lastUpdated || todo.lastUpdated > itemInServerData.lastUpdated) {
+      itemInServerData = { ...todo }
+    }
+
+    // update server item details in server todos array
+    sArr[index] = itemInServerData
+  })
+
+  // save updated data in local storage
+  setValue('todos', sArr)
 }

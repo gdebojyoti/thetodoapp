80be636c584fb04971887ba75bf1a777cd181214
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-10-21 23:35:08 +0530

    Route for Notes.

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './app/pages/Home'
+import Notes from './app/pages/Notes'
 import Settings from './app/pages/Settings'
 import Login from './app/pages/Login'
 
@@ -9,6 +10,7 @@ const Routes = () => {
   return (
     <BrowserRouter>
       <Route exact path='/' component={Home} />
+      <Route exact path='/notes' component={Notes} />
       <Route exact path='/settings' component={Settings} />
       <Route exact path='/login' component={Login} />
     </BrowserRouter>
---
+++ src/app/pages/Notes/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notes = () => {
+  return (
+    <div className='home'>
+      <h1 className='home__title'>My Notes</h1>
+
+      <div className='home__list'>Coming soon...!</div>
+    </div>
+  )
+}
+
+export default Notes
